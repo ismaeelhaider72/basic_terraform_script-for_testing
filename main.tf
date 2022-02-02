3c056c76d779bb86e3afc483148d8f9193554e13
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2022-02-03 01:49:48 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.small"
   key_name = "ismaeel_virginia_key"
-  vpc_security_group_ids = "sg-0cd4ec8eb25d1d425"
+  vpc_security_group_ids = ["sg-0cd4ec8eb25d1d425",]
   subnet_id = "subnet-0d9e6474140c1f6fc"
   associate_public_ip_address = true
 
