0af4d670332a7fea16816a387d2d79e16b8ef3d9
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2022-02-04 04:36:01 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,9 +22,8 @@ pipeline {
           
        stage('build user') {
           steps {
-            wrap([$class: 'BuildUser']) {
-              sh 'echo "${BUILD_USER}"'
-                }
+                BUILD_TRIGGER_BY=$(curl -k --silent ${env.BUILD_URL}/api/xml | tr '<' '\n' | egrep '^userId>|^userName>' | sed 's/.*>//g' | sed -e '1s/$/ \//g' | tr '\n' ' ')
+                echo "BUILD_TRIGGER_BY: ${BUILD_TRIGGER_BY}"
               }
             }
 
