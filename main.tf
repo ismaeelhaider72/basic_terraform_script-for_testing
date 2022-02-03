8e1c22dd5c567ce6b9aeffafa3ba083ff3e03112
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2022-02-03 22:32:35 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.small"
+  ami           = "var.imageId"
+  instance_type = "var.instanceType"
   key_name = "ismaeel_virginia_key"
   vpc_security_group_ids = ["sg-0cd4ec8eb25d1d425",]
   subnet_id = "subnet-0d9e6474140c1f6fc"
